013ff2da3bbef46bf6480669e59e7968c7bd84ab
shumizu418128 <84061748+shumizu418128@users.noreply.github.com>
2022-03-15 23:18:40 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -244,6 +244,7 @@ async def on_message(message):
                 file_names[i]), lang=lang, builder=pyocr.builders.TextBuilder(tesseract_layout=6))
             print(text2)
             all_text += text1 + text2
+        all_text = all_text.replace(' ', '')
         if "troubleshooting" in all_text:
             await channel.send("specific word found: troubleshooting")
             await channel.send(f"手動チェックに切替: {message.author.id}")
