03d329d8ac5d38df406c3aa9fac8a74992c823fd
shumizu418128 <84061748+shumizu418128@users.noreply.github.com>
2022-03-15 19:23:53 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -168,8 +168,7 @@ async def on_message(message):
 
     if len(message.attachments) == 2 and message.channel.id == 952946795573571654:  # 画像提出
         # 初期設定
-
-        pyocr.tesseract.TESSERACT_CMD = './.apt/usr/share/tesseract-ocr'
+    #    pyocr.tesseract.TESSERACT_CMD = './.apt/usr/share/tesseract-ocr'
         try:
             channel = await message.channel.create_thread(name=f"{message.author.display_name} 分析ログ", message=message)
         except AttributeError:
