dcb9354a4afe7fe0dea0860283d080ac80b07da3
shumizu418128 <84061748+shumizu418128@users.noreply.github.com>
2022-07-17 20:16:58 +0900

    Update discordbot_bf.py

--- discordbot_bf.py
+++ discordbot_bf.py
@@ -328,7 +328,7 @@ async def on_message(message):
         if member is None:
             all_names = []
             for mem in message.guild.members:
-                if not mem.bot and mem.id != 332886651107934219 and mem.id != 826371622084542474:  # RUSY, RUSY_2sub
+                if not mem.bot and mem.id not in [332886651107934219, 826371622084542474]:  # RUSY, RUSY_2sub
                     all_names.append(mem.display_name)
             all_names_edited = [normalize(mem).lower() for mem in all_names]
             results_edited = get_close_matches(
